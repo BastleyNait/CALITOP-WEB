d2883e62a2f5f3d5c9c8bbe6809663eb9664d1cb
BastleyNait <schirinosne@unsa.edu.pe>
2026-03-27 13:17:09 -0500

    Backup de mis cambios locales en nueva rama

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: process.env.R2_PUBLIC_DOMAIN?.split('/')[0] || "**.r2.dev",
-            },
-            {
-                protocol: "https",
-                hostname: "f005.backblazeb2.com",
-            },
-            {
-                protocol: "https",
-                hostname: "aceternity.com",
-            },
-            {
-                protocol: "https",
-                hostname: "images.unsplash.com",
-            },
-            {
-                protocol: "https",
-                hostname: "assets.aceternity.com",
-            },
-            {
-                protocol: "https",
-                hostname: "picsum.photos",
-            },
-        ],
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
+  /* config options here */
 };
 
 export default nextConfig;
